62e830abb407763d48186dad0de1b17d9ff0a0d0
priyanka singh <priyanka.singh.2907.2k.com>
2024-07-17 16:35:10 +0530

    Addedanimation

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,31 @@ import Services from "./Components/Services";
 import Recommend from "./Components/Recommend";
 import Testimonials from "./Components/Testimonials";
 import Footer from "./Components/Footer";
-
+import scrollreveal from "scrollreveal";
+import { useEffect } from "react";
 export default function App() {
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "top",
+      distance: "100px",
+      duration: 2000,
+      reset: true,
+    });
+    sr.reveal(
+      `
+        nav,
+        #hero,
+        #services,
+        #recommend,
+        #testimonials,
+        footer
+        `,
+      {
+        opacity: 0,
+        interval: 300,
+      }
+    );
+  }, []);
   return (
     <Main>
       <ScrollToTop />
